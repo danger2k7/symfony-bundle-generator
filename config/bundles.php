fc3ea98142f98381f741de909b5ff8999c50eb12
Bogdan SOOS <bogdan.soos@gmail.com>
2020-04-06 13:10:06 +0000

    Initial commit

---
+++ config/bundles.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle::class => ['all' => true],
+];
